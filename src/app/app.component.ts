bf5bf47745c350632d3e44c0e5a39192bbf6ca95
“Dana Elliott” <delliott123@comcast.net>
2019-05-13 16:14:00 -0400

    Update app to new host

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'RecipeApp';
+export class AppComponent implements  OnInit {
+  ngOnInit(): void {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBq6LPReiqjA8o48ppbPJuu5FOamlUugZ4',
+      authDomain: 'ng-recipe-book-1b2d9.firebaseapp.com'
+    });
+  }
 }
+
+
+
